3f4b72fff7ed3503314de9ea78b0d389e75219fe
Yoan Ante <yoanante@gmail.com>
2015-09-07 23:53:49 -0400

    Added playToFive() content. Crashing on a loopneed to review

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -79,6 +79,15 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins < 5 || computerWins < 5){
+      if ( getWinner(getPlayerMove(),getComputerMove()) == 'player'){
+        playerWins++;
+      }
+        else {
+          computerWins++;
+        }
+      console.log("player Chose" + getPlayerMove() + " computer chose" + getComputerMove());
+      console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
